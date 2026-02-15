3e094959e2ca4f1ee4da573607898565e12d437b
Brian <brian@grovina.com>
2026-02-15 16:37:16 +0000

    remove startup notification

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,11 @@
 import "./logger.js"; // Initialize logging first
-import { startBot, stopBot, sendMessageToOwner } from "./telegram.js";
+import { startBot, stopBot } from "./telegram.js";
 import { startHeartbeat, stopHeartbeat } from "./heartbeat.js";
 
 console.log("Brian starting up...");
 
 startBot();
 startHeartbeat();
-
-sendMessageToOwner("hey! back online").catch((err) => {
-  console.error("Failed to send startup message:", err);
-});
 
 process.on("SIGINT", shutdown);
 process.on("SIGTERM", shutdown);
